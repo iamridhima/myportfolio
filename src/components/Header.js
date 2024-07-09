58471ff95b77fa40b3ee5251deac6c11fed5cab9
Ridhima Bhatt <131640173+iamridhima@users.noreply.github.com>
2024-07-09 13:08:38 +0545

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from '../assets/logo.svg';
 
 const Header = () => {
   return (
-    <header className='py-8 -ml-20 mr-8'>
+    <header className='py-8 -ml-20 mr-8 mb-30'>
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
           <a href='#'>
