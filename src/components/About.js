0b9be43c1898a92f8c307a9dd53dfb8b47ba2fc9
Ridhima Bhatt <131640173+iamridhima@users.noreply.github.com>
2024-07-09 13:03:54 +0545

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
     threshold: 0.5,
   });
   return (
-    <section className='justify-between items-center -mr-12' id='about' ref={ref}>
+    <section className='justify-between items-center mt-30 -mr-12' id='about' ref={ref}>
       <div className='container mx-auto'>
         <div className='flex flex-col gap-56 gap-y-10 lg:flex-row'>
           <motion.div
