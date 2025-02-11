c2e1b4a98e4b2f386338e39eb265732cd02048c2
Ridhima Bhatt <131640173+iamridhima@users.noreply.github.com>
2025-02-11 18:18:47 +0545

    Update Work.js

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 
 import img1 from '../assets/1.png';
-import img2 from '../assets/2.png';
 import img3 from '../assets/3.png';
 import img4 from '../assets/4.png';
 import img5 from '../assets/5.png';
